109b8fd488eeeb695c8bbe9f7ea65f2e6ac48734
Rodrigo Pronunciati <65788897+rodpro-br@users.noreply.github.com>
2022-01-13 15:41:26 -0300

    Remoção URL MongoDB

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,4 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
-	MONGO_CONNECTION:
-		'mongodb+srv://urlshortenerdio:<password>@url-shortener-dio.kkxdw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+	MONGO_CONNECTION: ''
 }
