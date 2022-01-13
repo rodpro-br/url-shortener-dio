e3359adc2d895f534e86a8207e76035d5edcec32
Rodrigo Pronunciati <rodpro@hotmail.com>
2022-01-13 15:29:11 -0300

    remoção password da config do database

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		'mongodb+srv://urlshortenerdio:UrlSh0rtener@url-shortener-dio.kkxdw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+		'mongodb+srv://urlshortenerdio:<password>@url-shortener-dio.kkxdw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
